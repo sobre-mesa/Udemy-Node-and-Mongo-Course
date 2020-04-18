4320a17e57b5a027ae09f8f83eaad432d75158ef
sobre-mesa <sobremesa.software@gmail.com>
2020-04-18 12:49:57 +0530

    Get all/Get 1 implemented

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,14 +1,42 @@
 const fs = require('fs');
 const Tour = require('../models/tourModel');
 
-exports.getAllTours = (req, res) => {
-  return res.status(200).json(successfulRes({ tours }));
-}
+exports.getAllTours = async (req, res) => {
+  try {
+    const tours = await Tour.find();
+    res.status(200).json({
+      status: 'success',
+      results: tours.length,
+      data: {
+        tours
+      }
+    })
+  } 
+  catch (err) {
+    res.status(400).json({
+      status: 'failed',
+      message: err
+    })
+  }
+};
 
-exports.getTour = (req, res) => {
-  const id = req.params.id * 1;
-  const tour = tours.find(x => x.id === id);
-  return res.status(200).json(successfulRes({ tour }));
+exports.getTour = async (req, res) => {
+  const id = req.params.id;
+  try {
+    const tour = await Tour.findById(id);
+    res.status(200).json({
+      status: 'success',
+      data: {
+        tour
+      }
+    })
+  } 
+  catch (err) {
+    res.status(400).json({
+      status: 'failed',
+      message: err
+    })
+  }
 }
 
 exports.updateTour = (req, res) => {
